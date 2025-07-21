c0a0449e1ab24c857304e8f3778f1a2290fcd584
Lars Tieseler <tieseler@me.com>
2025-07-21 14:50:34 +0200

    feat: add img, add code in style.css and fonts

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,13 +1,12 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new Character();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
 
-    character.onload = () => {
-        ctx.drawImage(character, 0, 0, character.width, character.height, 0, 0, canvas.width, canvas.height);
-    }
+    console.log('My Character is', character);
+
+
 }
---
+++ models/character.class.js
@@ -0,0 +1,8 @@
+class Character extends MovableObject {
+
+
+
+    jump() {
+
+    }
+}
